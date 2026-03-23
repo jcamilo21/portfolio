4d6c1bcd875234436b3ce8a126c063e87ec0496e
JhonatanKamilo <jhonatancamilo1535@gmail.com>
2026-03-23 06:53:30 -0500

    update: red social

--- src/data/socials.data.ts
+++ src/data/socials.data.ts
@@ -5,7 +5,7 @@ import type { Social } from '@models';
 export const emailContact = 'jhonatancamilo1535@gmail.com';
 
 export const socials: Social[] = [
-  { icon: Github,   href: 'https://github.com/JhonatanKamilo/', angle: -60,  delay: 3000, bg: 'var(--blue-950)' },
+  { icon: Github,   href: 'https://github.com/jcamilo21', angle: -60,  delay: 3000, bg: 'var(--blue-950)' },
   { icon: Linkedin, href: 'https://www.linkedin.com/in/jcampos21/',          angle: 180,  delay: 3500, bg: 'var(--blue-200)' },
   { icon: LucideCodepen, href: 'https://codepen.io/jcampos21',             angle: 60,   delay: 4000, bg: 'var(--blue-950)'  },
 ];
